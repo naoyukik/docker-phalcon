e72e66585f7d93343e10cd7a81fdac5e934cb320
Naoyuki Kodama <mmaga+github@dstribe.net>
2022-01-23 12:32:08 +0900

    fix!: Dockerのマルチステージ対応
    - PHP/Phalcon/Xdebugのインストールを分離するため

--- infrastructure/docker/php/entrypoint.sh
+++ infrastructure/docker/php/entrypoint.sh
@@ -2,11 +2,12 @@
 
 # Exit the script if any statement returns a non-true return value
 set -e
-
-for file in /entrypoint.d/*.sh
-do
-  source "$file"
-done
+if [ -n "$(ls /entrypoint.d)" ]; then
+  for file in /entrypoint.d/*.sh
+  do
+    source "$file"
+  done
+fi
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
